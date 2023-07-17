8352195978753c16fe71078cbdbeffd7d6c9ee6a
Luigi <luigiscafi@gmail.com>
2023-07-17 16:41:57 +0200

    Add components: Hello and Identity

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello'
+import Identity from './components/Identity'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Hello/>
+          <Identity
+                name={'Luigi'}
+                bio={'lorem ipsum dolor sit amet, consector blablabla'}
+                />
+        </header>
+      </div>
   );
 }
 
-export default App;
---
+++ src/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function hello() {
+    const dwwm = "Hello les DWWM"
+    return (
+        <div>
+            {dwwm}
+        </div>
+
+    );
+
+}
